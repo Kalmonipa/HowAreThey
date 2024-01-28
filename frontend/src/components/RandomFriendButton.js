8aecb44f092fdf8dd516faf342697340f5fb9cbc
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2024-01-29 11:55:49 +1300

    tidy up randomfriendbutton

--- frontend/src/components/RandomFriendButton.js
+++ frontend/src/components/RandomFriendButton.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import '../css/RandomFriendButton.css'
+import React from "react";
+import "../css/RandomFriendButton.css";
 
+/**
+ * Creates a button that can be used to select a friend from the list
+ * @param {onRandomFriendSelect} onRandomFriendSelect
+ * @returns
+ */
 function RandomFriendButton({ onRandomFriendSelect }) {
-    return (
-        <button onClick={onRandomFriendSelect} className='random-friend-button'>
-            Pick a friend to contact
-        </button>
-    );
+  return (
+    <button onClick={onRandomFriendSelect} className="random-friend-button">
+      Pick a friend to contact
+    </button>
+  );
 }
 
 export default RandomFriendButton;
