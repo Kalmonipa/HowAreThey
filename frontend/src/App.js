4a1b929ea540687a6b30785e5273db495efc6ddd
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2024-01-29 17:19:26 +1300

    do some linting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import RandomFriendButton from './components/RandomFriendButton';
-import SearchBar from './components/SearchBar';
-import PageHeader from './components/PageHeader';
-import FriendTable from './components/FriendTable';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-import './css/FriendTable.css';
-import './css/App.css';
+import RandomFriendButton from "./components/RandomFriendButton";
+import SearchBar from "./components/SearchBar";
+import PageHeader from "./components/PageHeader";
+import FriendTable from "./components/FriendTable";
 
+import "./css/FriendTable.css";
+import "./css/App.css";
 
 function FilterableFriendsTable({ friends }) {
-  const [filterText, setFilterText] = useState('');
+  const [filterText, setFilterText] = useState("");
 
   const handleRandomFriend = () => {
-    fetch('http://localhost:8080/friends/random')
-      .then(response => {
+    fetch("http://localhost:8080/friends/random")
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
+          throw new Error("Network response was not ok ", +response.statusText);
         }
         return response.json();
       })
-      .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error,
+        );
       });
   };
 
   return (
-    <div className='body'>
+    <div className="body">
       <PageHeader />
       <div className="search-and-button">
-        <SearchBar
-          filterText={filterText}
-          onFilterTextChange={setFilterText}
-        />
+        <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
         <RandomFriendButton onRandomFriendSelect={handleRandomFriend} />
       </div>
-      <FriendTable
-        friends={friends}
-        filterText={filterText}
-      />
+      <FriendTable friends={friends} filterText={filterText} />
     </div>
   );
 }
+FilterableFriendsTable.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
 
 export default function App() {
   const [friends, setFriends] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:8080/friends')
-      .then(response => {
+    fetch("http://localhost:8080/friends")
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
-      .then(data => setFriends(data))
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .then((data) => setFriends(data))
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, []);
 
