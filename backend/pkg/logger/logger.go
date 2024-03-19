5616cce526ac0459596dd164397026459d9025b9
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2024-03-19 13:59:09 +1300

    logs to a file

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -7,7 +7,6 @@ import (
 	"time"
 )
 
-// Define log levels as constants
 const (
 	LogLevelDebug = iota
 	LogLevelInfo
@@ -16,12 +15,14 @@ const (
 	LogLevelFatal
 )
 
-// A variable to hold the minimum log level
 var minLogLevel = LogLevelInfo
+var fileLogger *log.Logger
 
 func SetupLogger() {
-	log.SetOutput(os.Stdout)
 	log.SetFlags(0)
+
+	// Configure stdout logger
+	log.SetOutput(os.Stdout)
 
 	if os.Getenv("TEST_ENV") == "true" {
 		minLogLevel = LogLevelFatal
@@ -45,9 +46,15 @@ func SetupLogger() {
 	default:
 		minLogLevel = LogLevelInfo
 	}
+
+	// Open log file
+	logFile, err := os.OpenFile("logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+	fileLogger = log.New(logFile, "", log.LstdFlags)
 }
 
-// logMessage function that supports formatted strings
 func LogMessage(level int, format string, args ...interface{}) {
 	ts := time.Now().Format("02/01/2006 15:04:05")
 	msg := fmt.Sprintf(format, args...)
@@ -70,5 +77,9 @@ func LogMessage(level int, format string, args ...interface{}) {
 		levelStr = "FATAL"
 	}
 
+	// Log to stdout
 	log.Printf("%s\t%s\t%s\n", ts, levelStr, msg)
+
+	// Log to file
+	fileLogger.Printf("%s\t%s\t%s\n", ts, levelStr, msg)
 }
