4b4f1662dbe91acec349d75be56e11c37f869a65
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2024-04-25 23:02:01 +1200

    integration test script

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -3,7 +3,7 @@ set -o errexit
 
 BRANCH_NAME=$(git branch --show-current)
 IMAGE_NAME="kalmonipa/howarethey:$BRANCH_NAME"
-CONTAINER_NAME="basic-test"
+CONTAINER_NAME="$BRANCH_NAME"
 
 ## Clean up
 cleanup() {
@@ -11,122 +11,29 @@ cleanup() {
     docker stop "$CONTAINER_NAME"
 }
 
-build_and_run() {
-    ## Build the image to test
-    #docker build . --tag "$IMAGE_NAME"
+# TODO: Get this bit working from within the Go tests so I can remove this script
+build_image() {
+    echo "Building image $IMAGE_NAME"
 
-    docker run --rm -p 8080:8080 --detach --name "$CONTAINER_NAME" "$IMAGE_NAME"
+    ## Build the image to test
+    docker build . --tag "$IMAGE_NAME"
 
     sleep 5
 }
 
-compare_responses() {
-    ACTUAL_RESPONSE=$1
-    EXPECTED_RESPONSE=$2
-    TEST_DESCRIPTION=$3
-
-    if [[ $ACTUAL_RESPONSE != $EXPECTED_RESPONSE ]]; then
-        echo "ERROR: $TEST_DESCRIPTION failed"
-        echo "  expected: $EXPECTED_RESPONSE"
-        echo "       got: $ACTUAL_RESPONSE"
-        exit 1
-    else
-        echo "INFO: $TEST_DESCRIPTION passed"
-    fi
-}
-
-## Function to add a friend
-add_friends() {
-    TODAYS_DATE=$(date '+%d/%m/%Y')
-
-    ACTUAL_RESPONSE=$(curl --silent --request POST \
-        --write-out "%{http_code}\n" \
-        --output /dev/null \
-        --data "{\"Name\":\"Jack Reacher\",\"LastContacted\":\"06/06/2023\",\"Birthday\":\"$TODAYS_DATE\"}" \
-        http://localhost:8080/friends)
-
-    EXPECTED_RESPONSE='201'
-
-    # Checking to make sure the friend addition worked properly
-    compare_responses $ACTUAL_RESPONSE $EXPECTED_RESPONSE "POST request to add a friend"
-}
-
-## Test functions
-
-
-
 main() {
 
-    add_friends
-
-    ## Test GET /friends
-    ACTUAL_RESPONSE=$(curl --silent http://localhost:8080/friends)
-
-    EXPECTED_RESPONSE="[{\"ID\":\"1\",\"Name\":\"Jack Reacher\",\"LastContacted\":\"06/06/2023\",\"Birthday\":\"$TODAYS_DATE\",\"Notes\":\"\"}]"
-
-    # TODO: Find out why doing a direct comparison fails the tests...
-    if diff <(echo "$EXPECTED_RESPONSE" | tr -d '[:space:]') <(echo "$ACTUAL_RESPONSE" | tr -d '[:space:]') >/dev/null; then
-        echo "INFO: GET request to retrieve friends passed"
-    else
-        echo "ERROR: GET request to retrieve friends failed"
-        echo "  expected: $EXPECTED_RESPONSE"
-        echo "       got: $ACTUAL_RESPONSE"
-        exit 1
-    fi
-
-    # Test DELETE /friends/:id
-    ACTUAL_RESPONSE=$(curl --silent --request DELETE \
-        --write-out "%{http_code}\n" \
-        --output /dev/null \
-        http://localhost:8080/friends/1
-    )
-
-    EXPECTED_RESPONSE='200'
-
-    compare_responses $ACTUAL_RESPONSE $EXPECTED_RESPONSE "DELETE request to remove a friend"
-
-    # Test GET /birthdays
-    TODAYS_DATE=$(date '+%d/%m/%Y')
-
-    ACTUAL_RESPONSE=$(curl --silent --request POST \
-        --write-out "%{http_code}\n" \
-        --output /dev/null \
-        --data "{\"Name\":\"Jack Reacher\",\"LastContacted\":\"06/06/2023\",\"Birthday\":\"$TODAYS_DATE\"}" \
-        http://localhost:8080/friends)
-
-    EXPECTED_RESPONSE='201'
-
-    # Checking to make sure the friend addition worked properly
-        if [[ $ACTUAL_RESPONSE != $EXPECTED_RESPONSE ]]; then
-        echo "ERROR: "POST request to add a friend failed"
-        echo "  expected: $EXPECTED_RESPONSE"
-        echo "       got: $ACTUAL_RESPONSE"
-        exit 1
-    else
-        echo "INFO: "POST request to add a friend passed"
-    fi
-
-    ACTUAL_RESPONSE=$(curl --silent \
-        --write-out "%{http_code}\n" \
-        --output /dev/null \
-        http://localhost:8080/birthdays
-    )
-
-    EXPECTED_RESPONSE='200'
-
-        # Checking to make sure the friend addition worked properly
-        if [[ $ACTUAL_RESPONSE != $EXPECTED_RESPONSE ]]; then
-        echo "ERROR: "GET request to get todays birthdays failed"
-        echo "  expected: $EXPECTED_RESPONSE"
-        echo "       got: $ACTUAL_RESPONSE"
-        exit 1
-    else
-        echo "INFO: "GET request to get todays birthdays passed"
-    fi
+    go test -v ./pkg/test/integration_test
 
 }
 
 trap cleanup EXIT
 
-build_and_run
+# Skip building the image to save time running tests
+if [[ "$BUILD_IMAGE" != "false" ]]; then
+    build_image
+else
+    echo "INFO: Skipping image build"
+fi
+
 main
